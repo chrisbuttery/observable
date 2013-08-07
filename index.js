c2d7cab69ea66d9c9a6cb95e7ef6c7835efadf07
Chris Buttery <info@chrisbuttery.com>
2013-08-07 13:44:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function Observable(obj){
   this.attributes = {};
 };
 
+// Give the observable the exact same events as backone
 _.extend(Observable.prototype, Backbone.Events);
 
+// if an object of events were passed through, add them to the Observable prototype
 function mixin(obj) {
   for (var key in Observable.prototype) {
     obj[key] = Observable.prototype[key];
@@ -12,6 +14,15 @@ function mixin(obj) {
   return obj;
 }
 
+// Set method
+// 1. if there isnt an options obj create an empty one
+// 2. check if the key is an object
+//    this.model.set({
+//      name: "bruce",
+//      age: "24"
+//    )};
+//
+// 3. loop thought object and pass through key, value, options to _set
 Observable.prototype.set = function(key, value, options) {
   options = options || {};
 
@@ -28,6 +39,9 @@ Observable.prototype.set = function(key, value, options) {
   return this;
 };
 
+// take the key, value, options values and set them on 'this.attributes' object
+// if silent is set to true .set('foo', 'bar', { silent: true }), don't emit an event.
+
 Observable.prototype._set = function(key, val, options) {
   options = options || {};
   var silent = options.silent || false;
@@ -41,10 +55,12 @@ Observable.prototype._set = function(key, val, options) {
   }
 };
 
+// get method
 Observable.prototype.get = function(key) {
   return this.attributes[key];
 };
 
+// call this.attributes in a callback() in a speific context
 Observable.prototype.each = function(callback, context) {
   _(this.attributes).each(callback, context);
 };
